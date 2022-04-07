b4b3834ee1df3bb9529485ff7b1d68f0c7485356
James Villarrubia <8172873+jamesvillarrubia@users.noreply.github.com>
2022-04-07 18:47:27 -0400

    change source again

--- runner.js
+++ runner.js
@@ -9,7 +9,7 @@ function init(config) {
 
     config = config || [];
     config.userName = config.userName || "james_mtc";
-    config.backgroundImageURL = config.backgroundImageURL || 'https://jamesvillarrubia.github.io/vigilant-goggles/empty_bg_devto.png';
+    config.backgroundImageURL = config.backgroundImageURL || 'https://jamesvillarrubia.github.io/vigilant-goggles/docs/assets/empty_bg_devto.png';
     config.quoteText = config.quoteText || "Standard Text if you don't select anything";
     config.articleURL = config.articleURL || "https://dev.to/inhuofficial/click-to-tweet-a-great-way-to-increase-traffic-generator-for-dev-to-5h49";
     config.fontSize = config.fontSize || 44;
